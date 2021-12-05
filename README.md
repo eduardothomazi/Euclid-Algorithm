5b5e8d7b9ef0c913dc5e6baae929d94346ac2aaf
Eduardo Thomazi <eduardothomazi91@gmail.com>
2021-12-05 11:20:56 -0300

    C1 - Solution with recursive and non recursive function

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Euclid-Algorithm
+
+It calculates the greatest common divisor between two numbers 'A' and 'B' 
+GCD(A,B)
+
+
+
--- src/Euclid.java
+++ src/Euclid.java
@@ -1,6 +1,34 @@
+
+
 public class Euclid {
     public static void main(String[] args) {
-        
+        System.out.print("Recursive -> " + gcdRecursive(5,5));
+        System.out.println();
+        System.out.print("Non recursive -> " + gcdNonRecursive(5,5));
+
+
+    }
+
+
+    public static int gcdRecursive(int number, int divisor){
+        int remaining = number % divisor;
+
+        if (remaining != 0){
+            return gcdRecursive(divisor, remaining);
+        }
+        else {
+            return divisor;
+        }
+    }
+
+    public static int gcdNonRecursive(int number, int divisor){
+        int remaining = number % divisor;
+        while (remaining != 0){
+            int newNumber = divisor;
+            divisor = remaining;
+            remaining = newNumber % divisor;
+        }
+        return divisor;
     }
 
 }
